ae26a842201f57da2372cf8a842df0ae3f40f198
Kim Feuerbacher <kim.feuerbacher@gmail.com>
2022-08-23 15:39:10 +0200

    new and create action for rides

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name,:last_name,:email,:password, presence: true
+  validates :email, :password, presence: true
   validates :email, uniqueness: true
-
 end
