6df8de03881447af0f7c30bbb8a3410e2048aaf2
Thais Paiva <hello@thaispaiva.info>
2022-08-25 11:45:11 +0200

    fixed devise on rides controller, added container on login form

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,5 +1,8 @@
 class RidesController < ApplicationController
   before_action :set_ride, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @rides = Ride.all
     if params['search']
