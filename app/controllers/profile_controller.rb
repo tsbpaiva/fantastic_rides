2f67d8b85358189afb11ef50fa8147bdb09099df
Thais Paiva <hello@thaispaiva.info>
2022-08-25 17:48:19 +0200

    user can see bookings they requested

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,9 @@
 class ProfileController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @user = current_user
     @rides = current_user.rides
+    @bookings = current_user.bookings
   end
 end
